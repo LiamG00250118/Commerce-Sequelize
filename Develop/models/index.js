09a36fccaa07581fbf2e9e5f733791ade5b0abd9
Liamb44 <liamrbrandt@gmail.com>
2023-08-20 16:22:52 -0400

    final submit

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,27 +1,8 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-class Category extends Model {}
-
-Category.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  category_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-}, {
-  sequelize,
-  modelName: 'category'
-});
 
 module.exports = {
   Product,
